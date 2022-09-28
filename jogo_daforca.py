8466719a48e636c166fc66f974984bcaeaa6cbb8
carolina leal <65249499+carolina-ml@users.noreply.github.com>
2022-09-28 20:33:51 -0300

    create jogo da forca inicio

---
+++ jogo_daforca.py
@@ -0,0 +1,153 @@
+import random as r
+from turtle import clear
+import os
+import time
+
+lista_palavras = ['laranja', 'curitiba', 'cachorro', 'programador', 'desembargador', 'quiroprata',
+                  'escaravelho', 'pelicano', 'candelabro', 'vuvuzela', 'advogado', 'bumerangue', 'grampeador']
+lista_erros = list()
+lista_acertos = list()
+erros = 7
+acertos = 0
+
+palavra_selecionada = r.choice(lista_palavras)
+lista_letras = list()
+lista_apresentacao = list()
+
+
+def palavraEscolhida():
+    print(f'Palavra escolhida é {palavra_selecionada}')
+
+
+nome = input('Olá, qual o seu nome?')
+
+
+def saudacao():
+    print(f'Olá {nome}!, Seja bem-vindo(a) ao jogo da Forca! :)')
+
+
+def quantidadeLetras():
+    print(f'Sua palavra tem {len(palavra_selecionada)} letras !')
+
+
+for i in range(len(palavra_selecionada)):
+    lista_letras.append(palavra_selecionada[i].lower())
+
+for i in range(len(palavra_selecionada)):
+    lista_apresentacao.append('_')
+
+
+def tela():
+    print(f'Você so pode errar {erros} vezes até acertar a palavra!')
+    
+    if len(lista_acertos) > 0:
+        for i in range(len(lista_acertos)):
+            numElementos = lista_letras.count(lista_acertos[i])
+            for elemento in range(numElementos):
+                indice = lista_letras.index(lista_acertos[i])
+                lista_letras.pop(indice)
+                lista_letras.insert(indice, '$')
+                lista_apresentacao.pop(indice)
+                lista_apresentacao.insert(indice, lista_acertos[i])
+
+    # for i in range(len(lista_apresentacao)):
+    #     print(lista_apresentacao[i])
+
+    print(lista_apresentacao)
+
+
+def tentativa(letra):
+    if palavra_selecionada.lower().find(letra) != -1:
+        if (lista_acertos.__contains__(letra)):
+            print('Essa letra já foi')
+            # print(lista_acertos)
+        else:
+            lista_acertos.append(letra)
+            print(f'Woow!, A letra {letra} existe na sua palavra! Muito bem !')
+
+        return 0
+    else:
+        if (lista_erros.__contains__(letra)):
+            print(f'Essa letra já foi')
+            return 0
+        else:
+            lista_erros.append(letra)
+            print(
+                f'Ahhh!, A letra {letra} não existe na sua palavra! Tente novamente !')
+            return 1
+
+def bonecoErro():
+    if erros == 6:
+        print('_______ ')
+        print('|   o   ')
+        print('|       ')
+        print('|       ')
+        print('|______ ')
+    elif erros == 5:
+        print('_______ ')
+        print('|   o   ')
+        print('|   |   ')
+        print('|       ')
+        print('|______ ')
+    elif erros == 4:
+        print('_______ ')
+        print('|  \o   ')
+        print('|   |   ')
+        print('|       ')
+        print('|______ ')
+    elif erros == 3:
+        print('_______ ')
+        print('|  \o/  ')
+        print('|   |   ')
+        print('|       ')
+        print('|______ ')
+    elif erros == 2:
+        print('_______ ')
+        print('|  \o/  ')
+        print('|   |   ')
+        print('|    \  ')
+        print('|______ ')
+    elif erros == 1:
+        print('_______ ')
+        print('|  \o/  ')
+        print('|   |   ')
+        print('|  / \  ')
+        print('|______ ')
+
+    print(f'Letras erradas: {lista_erros}')
+        
+def acertou():
+    if (lista_letras.count('$') == len(palavra_selecionada)):
+        os.system('cls')
+        time.sleep(1)
+        print('Você acertou a palavra, PARABENS!!!')
+        print(f'A palavra era {palavra_selecionada}!')
+        return True
+    else:
+        return False
+
+saudacao()
+
+i = 1
+while i <= erros:
+    #palavraEscolhida()
+    quantidadeLetras()
+    tela()
+    bonecoErro()
+    if acertou():
+        break
+
+    letra = input('Escolha uma letra:')
+    erros -= tentativa(letra.lower())
+    time.sleep(2)
+    os.system('cls')
+
+if (erros == 0):
+    print('_______ ')
+    print('|   |   ')
+    print('|   o   ')
+    print('|  /|\   ')
+    print('|  | |  ')
+    print('|______ ')
+    print(f'Que pena você errou! Tente novamente')
+    time.sleep(2)
